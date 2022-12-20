ee4facb73c7077a49b192e61b1ebf097c2d4d1ce
fushigikabu <kayamin0211@gmail.com>
2022-12-20 18:10:59 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true
+  validates :last_name,          presence: true
+  validates :first_name_reading, presence: true
+  validates :first_name_reading, presence: true
+  validates :birthday,           presence: true
+
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+             message: "は半角6文字以上、英字・数字それぞれ１文字以上含む必要があります"}
+
+
+
 end
