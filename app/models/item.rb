0103452351c89e9d94942dfdbe6ceaee7219ff75
fushigikabu <kayamin0211@gmail.com>
2022-12-23 22:15:32 +0900

    商品情報がデータベースに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,11 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_time
 
-  validates :name, :text, presence: true
+  has_one_attached :image
+
+  validates :name, :text, :image, presence: true
   validates :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
 end
 
