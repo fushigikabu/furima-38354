e4fc03246538ed92d6929d47df33ec356be74302
fushigikabu <kayamin0211@gmail.com>
2022-12-23 23:16:40 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,11 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: :new
 
   def index
   end
 
   def new
-    @item=Item.new
+    @item = Item.new
   end
 
   def create
@@ -18,9 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
- private
+  private
 
- def item_params
-  params.require(:item).permit(:name, :text, :image, :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, :item_price).merge(user_id: current_user.id)
- end
+  def item_params
+    params.require(:item).permit(:name, :text, :image, :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id,
+                                 :delivery_time_id, :item_price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, :text, :image, presence: true
-  validates :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
-
+  validates :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_time_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
-
