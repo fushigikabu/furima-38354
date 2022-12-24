f9928be7ab5fe83a3bdd294531e3a73afe0cb0cc
fushigikabu <kayamin0211@gmail.com>
2022-12-24 14:57:47 +0900

    テストコード修正
    item_priceに半角数字以外が含まれている場合は出品できない
    追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,6 +58,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Item price is not a number')
       end
+      it 'item_priceに半角数字以外が含まれている場合は出品できない' do
+        @item.item_price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is not a number')
+      end
       it 'item_priceは、300以上でないと登録出来ない' do
         @item.item_price = 200
         @item.valid?
