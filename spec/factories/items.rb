9b61d10696fff3b6d3ced285d51b4c0db84c3ca4
fushigikabu <kayamin0211@gmail.com>
2022-12-24 14:46:36 +0900

    テスト用ダミー画像 訂正
    参考カリキュラムを元に訂正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
+
   factory :item do
+
     name { Faker::Name.name }
     text { Faker::Lorem.sentence }
     genre_id { Faker::Number.between(from: 2, to: 11) }
@@ -8,5 +10,13 @@ FactoryBot.define do
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     delivery_time_id { Faker::Number.between(from: 2, to: 4) }
     item_price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
+
 end
