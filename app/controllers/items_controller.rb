40d58d47e9c0b53df31f93919414df3da87f7526
fushigikabu <kayamin0211@gmail.com>
2022-12-25 17:14:32 +0900

    同じ記述まとめる
    before_actionに同じ記述をまとめると可読性が上がる為

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :update, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,12 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -33,7 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
@@ -41,5 +39,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :image, :genre_id, :item_condition_id, :delivery_charge_id, :prefecture_id,
                                  :delivery_time_id, :item_price).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
